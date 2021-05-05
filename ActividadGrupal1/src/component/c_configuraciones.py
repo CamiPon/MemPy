10f47e288dcdb084aaf30fc08d252cfb1fbfc4ab
CamiPon <camilayelenponce@gmail.com>
2021-05-05 19:05:46 -0300

    Configuraciones funcionando

--- ActividadGrupal1/src/component/c_configuraciones.py
+++ ActividadGrupal1/src/component/c_configuraciones.py
@@ -13,9 +13,12 @@ def loop():
     conf = configuracion(username)
     window = v_configuraciones.build(conf.buscarConfig())
     while True:
-        event, _values = window.read()
+        event, values = window.read()
 
         if event in (sg.WIN_CLOSED,"Exit", "-VOLVER-"):
             break
-        
+        if event == "-GUARDAR-":
+            conf = configuracion(username, "", values["-CASILLAS-"], values["-COINCIDENCIAS-"], values["-TIEMPO-"], values["-ESTILO-"], values["-ELEMENTOS-"], values["-AYUDAS-"])
+            conf.guardarJson()
+            break
     return window
--- ActividadGrupal1/src/model/configuracion.py
+++ ActividadGrupal1/src/model/configuracion.py
@@ -3,33 +3,41 @@ import json
 import os
 class configuracion():
 
-    def __init__(self,username,textos = "", cant_casillas = "8x8", tiempo = 120, estilo = "Predeterminado", tipo_elementos = "Ambos", ayudas = "No"):
-        self.username = username,
-        self.textos = textos,
-        self.cant_casillas = cant_casillas,
-        self.tiempo = tiempo,
-        self.estilo = estilo,
-        self.tipo_elementos = tipo_elementos,
-        self.ayudas = ayudas,
+    def __init__(self,username,textos = "", cant_casillas = "8x8", coincidencias = 2, tiempo = 120, estilo = "Predeterminado", tipo_elementos = "Ambos", ayudas = "No"):
+        self.username = username
+        self.textos = textos
+        self.cant_casillas = cant_casillas
+        self.coicidencias = coincidencias
+        self.tiempo = tiempo
+        self.estilo = estilo
+        self.tipo_elementos = tipo_elementos
+        self.ayudas = ayudas
         
     def guardarJson(self):
         try:
-            with open("archivos"+ os.sep +"arc_configuracion.json", encoding="utf8") as arc_configuracion:
+            with open("Archivos"+ os.sep +"arc_configuracion.json","r", encoding="utf8") as arc_configuracion:
                 data_configuracion = json.load(arc_configuracion)
         except:
                 data_configuracion= []
-        with open("Archivos"+ os.sep +"arc_configuracion.json", "w", encoding="utf8") as file:
-            data = {
+        data = {
                 self.username : {
                 "textos" : self.textos,
-                "cantidad de casillas" : self.cant_casillas,
-                "tiempo maximo" : self.tiempo,
+                "cant_casillas" : self.cant_casillas,
+                "coincidencias" : self.coicidencias,
+                "tiempo" : self.tiempo,
                 "estilo" : self.estilo,
-                "tipo elementos" : self.tipo_elementos,
+                "tipo_elementos" : self.tipo_elementos,
                 "ayudas" : self.ayudas,
                 }
             }
-            data_configuracion.append(data)
+        with open("Archivos"+ os.sep +"arc_configuracion.json", "w", encoding="utf8") as file:
+            i = 0
+            while(i<len(data_configuracion) and self.username not in data_configuracion[i]):
+                i = i + 1
+            if i < len(data_configuracion):
+                data_configuracion[i] = data
+            else:
+                data_configuracion.append(data)
             json.dump(data_configuracion,file, indent=4, ensure_ascii=False)
 
     def buscarConfig(self):
@@ -44,9 +52,18 @@ class configuracion():
                     self.username,
                     aux[self.username]["textos"],
                     aux[self.username]["cant_casillas"],
+                    aux[self.username]["coincidencias"],
                     aux[self.username]["tiempo"],
                     aux[self.username]["estilo"],
-                    aux[self.username]["tipo elementos"],
+                    aux[self.username]["tipo_elementos"],
                     aux[self.username]["ayudas"]
                     )
         return self
+    def imprimir(self):
+        print(self.username)
+        print(self.cant_casillas)
+        print(self.coicidencias)
+        print(self.tipo_elementos)
+        print(self.estilo)
+        print(self.tiempo)
+        print(self.textos)
